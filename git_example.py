92e160eef94b2e4540fc6627dd3624ef764a9267
Vladimir Mikhailove <vova.mikhailov2005@list.ru>
2023-12-22 04:17:20 +0500

    Vtoroe soxranenie

--- git_example.py
+++ git_example.py
@@ -1,5 +1,24 @@
 class Cat:
     def __init__(self, name):
         self.name = name
+
     def say(self):
         print('Мяу, - сказал', self.name)
+
+    def change_name(self, new_name):
+        if len(new_name) >= 2:
+            self.name = new_name
+        else:
+            print('Плохое имя')
+
+
+cat1 = Cat('Вася')
+cat2 = Cat('Борис')
+
+cat1.say()
+cat2.say()
+
+cat1.change_name('Снежок')
+
+cat1.say()
+cat2.say()
